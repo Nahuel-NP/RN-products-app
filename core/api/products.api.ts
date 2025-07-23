44c1561250f04ba27bc5ba9f938a634e2418145d
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-07-23 13:53:17 -0300

    feat: Configures API URL and environment variables
    Sets up environment variables for different API URLs based on the stage
    and platform.
    
    Adds a `.env.template` file and ignores `.env` to manage sensitive
    information.
    
    Updates the `README.md` to reflect changes.
    
    Fixes #123

--- core/api/products.api.ts
+++ core/api/products.api.ts
@@ -1,8 +1,17 @@
 import axios from "axios";
-//TODO: conectar mediante envs vars, android  e Ios
+import { Platform } from "react-native";
 
+const STAGE = process.env.EXPO_PUBLIC_STAGE || "dev";
+export const API_URL =
+  STAGE === "dev"
+    ? process.env.EXPO_PUBLIC_API_URL
+    : Platform.OS === "web"
+    ? process.env.EXPO_PUBLIC_API_URL_IOS
+    : process.env.EXPO_PUBLIC_API_URL_ANDROID;
+
+console.log({ STAGE, [Platform.OS]: API_URL });
 const productsApi = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: API_URL,
 });
 
 //TODO: interceptores
