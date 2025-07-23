a6283fc291c9ab534eaba062a0056fcc88e4c634
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-07-23 12:00:54 -0300

    init products-app project

--- presentation/theme/components/ParallaxScrollView.tsx
+++ presentation/theme/components/ParallaxScrollView.tsx
@@ -6,11 +6,8 @@ import Animated, {
   useAnimatedStyle,
   useScrollViewOffset,
 } from 'react-native-reanimated';
-
-import { ThemedView } from '@/components/ThemedView';
-import { useBottomTabOverflow } from '@/components/ui/TabBarBackground';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
+import { useColorScheme } from '../hooks/useColorScheme';
+import { ThemedView } from './ThemedView';
 const HEADER_HEIGHT = 250;
 
 type Props = PropsWithChildren<{
@@ -26,7 +23,7 @@ export default function ParallaxScrollView({
   const colorScheme = useColorScheme() ?? 'light';
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
   const scrollOffset = useScrollViewOffset(scrollRef);
-  const bottom = useBottomTabOverflow();
+
   const headerAnimatedStyle = useAnimatedStyle(() => {
     return {
       transform: [
@@ -46,11 +43,7 @@ export default function ParallaxScrollView({
 
   return (
     <ThemedView style={styles.container}>
-      <Animated.ScrollView
-        ref={scrollRef}
-        scrollEventThrottle={16}
-        scrollIndicatorInsets={{ bottom }}
-        contentContainerStyle={{ paddingBottom: bottom }}>
+      <Animated.ScrollView ref={scrollRef} scrollEventThrottle={16}>
         <Animated.View
           style={[
             styles.header,
