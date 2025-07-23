346e437be7b63e33a86229cbdc9ca8126a40aec4
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-07-23 13:35:06 -0300

    implement auth actions

---
+++ core/actions/auth-actions.ts
@@ -0,0 +1,54 @@
+import { productsApi } from "../api/products.api";
+import { User } from "../auth/interface/user";
+
+export interface AuthResponse {
+  id: string;
+  email: string;
+  fullName: string;
+  isActive: boolean;
+  roles: string[];
+  token: string;
+}
+
+const returnUserToken = (data: AuthResponse): { user: User; token: string } => {
+  const { token, ...user } = data;
+  /*  const { token, id, email, fullName, isActive, roles } = data;
+
+  const user: User = {
+    id,
+    email,
+    fullName,
+    isActive,
+    roles,
+  }; */
+
+  return { token, user };
+};
+
+export const authLogin = async (email: string, password: string) => {
+  const emailLower = email.toLowerCase();
+  try {
+    const { data } = await productsApi.post<AuthResponse>("/auth/login", {
+      emailLower,
+      password,
+    });
+    return returnUserToken(data);
+  } catch (error) {
+    console.log(error);
+    // throw new Error("Error al iniciar sesión");
+    return null;
+  }
+};
+
+export const checkAuthStatus = async () => {
+  try {
+    const { data } = await productsApi.get<AuthResponse>("/auth/check-status");
+    return returnUserToken(data);
+  } catch (error) {
+    console.log(error);
+    // throw new Error("Error al verificar sesión");
+    return null;
+  }
+};
+
+//TODO: register
--- presentation/auth/store/useAuthStore.ts
+++ presentation/auth/store/useAuthStore.ts
@@ -1,3 +1,4 @@
+import { authLogin, checkAuthStatus } from "@/core/actions/auth-actions";
 import { User } from "@/core/auth/interface/user";
 import { create } from "zustand";
 
@@ -18,12 +19,30 @@ export const useAuthStore = create<AuthState>()((set) => ({
   token: undefined,
   user: undefined,
   login: async (email: string, password: string) => {
+    const response = await authLogin(email, password);
+    if (!response) {
+      set({ status: "unauthenticated" });
+      return false;
+    }
+    const { token, user } = response;
+    set({ status: "authenticated", token, user });
+
+    //TODO: guardar token en secure storage
     return true;
   },
   logout: async () => {
+    //TODO: borrar token de secure storage
+    set({ status: "unauthenticated", token: undefined, user: undefined });
     return;
   },
   checkStatus: async () => {
+    const resp = await checkAuthStatus();
+    if (!resp) {
+      set({ status: "unauthenticated" });
+      return;
+    }
+    const { token, user } = resp;
+    set({ status: "authenticated", token, user });
     return;
   },
 }));
