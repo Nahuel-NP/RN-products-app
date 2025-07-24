4afc3d17c59108cc3a50a8ba4470c80f5a849360
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-07-24 14:12:31 -0300

    fix scroll

--- app/auth/login/index.tsx
+++ app/auth/login/index.tsx
@@ -1,60 +1,115 @@
+import { useAuthStore } from "@/presentation/auth/store/useAuthStore";
 import ThemedButton from "@/presentation/theme/components/ThemedButton";
 import ThemedLink from "@/presentation/theme/components/ThemedLink";
 import { ThemedText } from "@/presentation/theme/components/ThemedText";
 import ThemedTextInput from "@/presentation/theme/components/ThemedTextInput";
 import { useThemeColor } from "@/presentation/theme/hooks/useThemeColor";
-import { KeyboardAvoidingView, ScrollView, View } from "react-native";
+import { router } from "expo-router";
+import { useState } from "react";
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  View,
+} from "react-native";
 
 const LoginScreen = () => {
   const backgroundColor = useThemeColor({}, "background");
 
+  const { login } = useAuthStore();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [isPosting, setIsPosting] = useState(false);
+
+  const handleLogin = async () => {
+    if (form.email.length === 0 || form.password.length === 0) {
+      return;
+    }
+    console.log(form);
+    setIsPosting(true);
+    const wasSuccess = await login(form.email, form.password);
+    setIsPosting(false);
+    if (wasSuccess) {
+      router.replace("/");
+      return;
+    }
+
+    Alert.alert("Error", "Usuario o contraseña incorrectos");
+  };
+
   return (
-    <KeyboardAvoidingView style={{ flex: 1 }}>
-      <ScrollView
-        style={{ backgroundColor }}
-        contentContainerStyle={{
-          justifyContent: "center",
-          flex: 1,
-          paddingHorizontal: 20,
-        }}
+    <SafeAreaView style={{ flex: 1, backgroundColor }}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
-        <View style={{}}>
-          <ThemedText type="title">Ingresar</ThemedText>
-          <ThemedText style={{ color: "#bbb" }} type="subtitle">
-            Por favor ingrese para continuar
-          </ThemedText>
-          <View style={{ marginVertical: 20, gap: 10 }}>
-            <ThemedTextInput
-              placeholder="Correo Electronico"
-              keyboardType="email-address"
-              autoCapitalize="none"
-              icon="mail-outline"
-            />
-            <ThemedTextInput
-              placeholder="Contraseña"
-              keyboardType="default"
-              autoCapitalize="none"
-              secureTextEntry
-              icon="lock-closed-outline"
-            />
-          </View>
-          <ThemedButton
-            postPendIcon="arrow-forward-outline"
-            onPress={() => {
-              console.log("Ingresar");
-            }}
-          >
-            Ingresar
-          </ThemedButton>
-          <View style={{ marginTop: 20, flexDirection: "row", alignItems: "center", gap: 10, justifyContent: "center" }}>
-            <ThemedText style={{ color: "white",fontSize: 18 }} type="default">
-              ¿No tienes cuenta?
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{
+            flexGrow: 1,
+            justifyContent: "center",
+            paddingHorizontal: 20,
+            paddingBottom: 20, // Evita espacio vacío al cerrar el teclado
+          }}
+        >
+          <View>
+            <ThemedText type="title">Ingresar</ThemedText>
+            <ThemedText style={{ color: "#bbb" }} type="subtitle">
+              Por favor ingrese para continuar
             </ThemedText>
-            <ThemedLink push href="/auth/register" style={{ fontSize: 18 }}>Crear cuenta</ThemedLink>
+
+            <View style={{ marginVertical: 20, gap: 10 }}>
+              <ThemedTextInput
+                placeholder="Correo Electronico"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                icon="mail-outline"
+                value={form.email}
+                onChangeText={(text) => setForm({ ...form, email: text })}
+              />
+              <ThemedTextInput
+                placeholder="Contraseña"
+                keyboardType="default"
+                autoCapitalize="none"
+                secureTextEntry
+                icon="lock-closed-outline"
+                value={form.password}
+                onChangeText={(text) => setForm({ ...form, password: text })}
+              />
+            </View>
+
+            <ThemedButton
+              disabled={isPosting}
+              postPendIcon="arrow-forward-outline"
+              onPress={handleLogin}
+            >
+              Ingresar
+            </ThemedButton>
+
+            <View
+              style={{
+                marginTop: 20,
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                justifyContent: "center",
+              }}
+            >
+              <ThemedText style={{ color: "white", fontSize: 18 }} type="default">
+                ¿No tienes cuenta?
+              </ThemedText>
+              <ThemedLink push href="/auth/register" style={{ fontSize: 18 }}>
+                Crear cuenta
+              </ThemedLink>
+            </View>
           </View>
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
--- app/auth/register/index.tsx
+++ app/auth/register/index.tsx
@@ -3,71 +3,94 @@ import ThemedLink from "@/presentation/theme/components/ThemedLink";
 import { ThemedText } from "@/presentation/theme/components/ThemedText";
 import ThemedTextInput from "@/presentation/theme/components/ThemedTextInput";
 import { useThemeColor } from "@/presentation/theme/hooks/useThemeColor";
-import { KeyboardAvoidingView, ScrollView, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  View,
+} from "react-native";
 
 const RegisterScreen = () => {
   const backgroundColor = useThemeColor({}, "background");
 
   return (
-    <KeyboardAvoidingView style={{ flex: 1 }}>
-      <ScrollView
-        style={{ backgroundColor }}
-        contentContainerStyle={{
-          justifyContent: "center",
-          flex: 1,
-          paddingHorizontal: 20,
-        }}
+    <SafeAreaView style={{ flex: 1, backgroundColor }}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 0}
       >
-        <View style={{}}>
-          <ThemedText type="title">Crear Cuenta</ThemedText>
-          <ThemedText style={{ color: "#bbb" }} type="subtitle">
-            Para continuar, cree una cuenta.
-          </ThemedText>
-          <View style={{ marginVertical: 20, gap: 10 }}>
-            <ThemedTextInput
-              placeholder="Nombre completo"
-              keyboardType="default"
-              autoCapitalize="none"
-              icon="person-outline"
-            />
-            <ThemedTextInput
-              placeholder="Correo Electronico"
-              keyboardType="email-address"
-              autoCapitalize="none"
-              icon="mail-outline"
-            />
-            <ThemedTextInput
-              placeholder="Contraseña"
-              keyboardType="default"
-              autoCapitalize="none"
-              secureTextEntry
-              icon="lock-closed-outline"
-            />
-            <ThemedTextInput
-              placeholder="Confirmar contraseña"
-              keyboardType="default"
-              autoCapitalize="none"
-              secureTextEntry
-              icon="lock-closed-outline"
-            />
-          </View>
-          <ThemedButton
-            postPendIcon="arrow-forward-outline"
-            onPress={() => {
-              console.log("Registrar");
-            }}
-          >
-            Registrar
-          </ThemedButton>
-          <View style={{ marginTop: 20, flexDirection: "row", alignItems: "center", gap: 10, justifyContent: "center" }}>
-            <ThemedText style={{ color: "white",fontSize: 18 }} type="default">
-              ¿Ya tienes cuenta?
+        <ScrollView
+          contentContainerStyle={{
+            flexGrow: 1,
+            justifyContent: "center",
+            paddingHorizontal: 20,
+          }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View>
+            <ThemedText type="title">Crear Cuenta</ThemedText>
+            <ThemedText style={{ color: "#bbb" }} type="subtitle">
+              Para continuar, cree una cuenta.
             </ThemedText>
-            <ThemedLink dismissTo href="/auth/login" style={{ fontSize: 18 }}>Iniciar sesión</ThemedLink>
+
+            <View style={{ marginVertical: 20, gap: 10 }}>
+              <ThemedTextInput
+                placeholder="Nombre completo"
+                keyboardType="default"
+                autoCapitalize="none"
+                icon="person-outline"
+              />
+              <ThemedTextInput
+                placeholder="Correo Electronico"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                icon="mail-outline"
+              />
+              <ThemedTextInput
+                placeholder="Contraseña"
+                keyboardType="default"
+                autoCapitalize="none"
+                secureTextEntry
+                icon="lock-closed-outline"
+              />
+              <ThemedTextInput
+                placeholder="Confirmar contraseña"
+                keyboardType="default"
+                autoCapitalize="none"
+                secureTextEntry
+                icon="lock-closed-outline"
+              />
+            </View>
+
+            <ThemedButton
+              postPendIcon="arrow-forward-outline"
+              onPress={() => console.log("Registrar")}
+            >
+              Registrar
+            </ThemedButton>
+
+            <View
+              style={{
+                marginTop: 20,
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                justifyContent: "center",
+              }}
+            >
+              <ThemedText style={{ color: "white", fontSize: 18 }} type="default">
+                ¿Ya tienes cuenta?
+              </ThemedText>
+              <ThemedLink dismissTo href="/auth/login" style={{ fontSize: 18 }}>
+                Iniciar sesión
+              </ThemedLink>
+            </View>
           </View>
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
