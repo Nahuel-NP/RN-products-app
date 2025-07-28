e106b6bdd21c177e7ee967279575d043876f39dc
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-07-28 14:58:20 -0300

    create product details page

---
+++ app/(products-app)/product/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const ProductScreen = () => {
+  return (
+    <View>
+      <Text>[id]</Text>
+    </View>
+  )
+}
+
+export default ProductScreen
