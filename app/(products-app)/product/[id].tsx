4e91931c5bd0a16bf3bcfed19b03d14afcb57222
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-07-29 15:38:23 -0300

    create camera screen and menu cam icon

--- app/(products-app)/product/[id].tsx
+++ app/(products-app)/product/[id].tsx
@@ -1,13 +1,18 @@
 import ProductImages from "@/presentation/products/components/ProductImages";
 import { useProduct } from "@/presentation/products/hooks/useProduct";
 import CustomKeyboardAvoidingView from "@/presentation/shared/CustomKeyboardAvoidVIew";
+import MenuIconButton from "@/presentation/theme/components/MenuIconButton";
 import ThemedButton from "@/presentation/theme/components/ThemedButton";
 import ThemedButtonGroup from "@/presentation/theme/components/ThemedButtonGroup";
 import { ThemedText } from "@/presentation/theme/components/ThemedText";
 import ThemedTextInput from "@/presentation/theme/components/ThemedTextInput";
 import { ThemedView } from "@/presentation/theme/components/ThemedView";
-import { Ionicons } from "@expo/vector-icons";
-import { Redirect, useLocalSearchParams, useNavigation } from "expo-router";
+import {
+  Redirect,
+  router,
+  useLocalSearchParams,
+  useNavigation,
+} from "expo-router";
 import { Formik } from "formik";
 import React, { useEffect } from "react";
 import { ActivityIndicator, ScrollView, View } from "react-native";
@@ -21,7 +26,10 @@ const ProductScreen = () => {
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <Ionicons name="camera-outline" size={24} color="black" />
+        <MenuIconButton
+          icon="camera-outline"
+          onPress={() => router.push("/camera")}
+        />
       ),
     });
   }, [navigation]);
