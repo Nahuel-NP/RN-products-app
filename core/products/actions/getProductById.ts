ea327a511e011d9a4b48ba6c9db7e615bfb9bbd3
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-07-29 12:50:55 -0300

    return empty product to create

--- core/products/actions/getProductById.ts
+++ core/products/actions/getProductById.ts
@@ -1,7 +1,22 @@
 import { API_URL, productsApi } from "@/core/api/products.api";
-import { type Product } from "../interface/product.interface";
+import { Gender, type Product } from "../interface/product.interface";
 
+const emptyProduct: Product = {
+  id: "",
+  title: "New product",
+  slug: "new-product",
+  description: "",
+  images: [],
+  price: 0,
+  stock: 0,
+  sizes: [],
+  gender: Gender.Men,
+  tags: [],
+};
 export const getProductById = async (id: string) => {
+  if (id === "new") {
+    return emptyProduct;
+  }
   try {
     const { data } = await productsApi.get<Product>(`/products/${id}`);
     return {
