4dc15d140b87f419ef081ae8ab80b7005326f93a
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-08-11 17:04:49 -0300

    create useCameraStore

---
+++ presentation/store/useCameraStore.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+
+interface TemporalCameraStoreState {
+  selectedImage: string[];
+  addSelectedImage: (image: string) => void;
+  clearImages: () => void;
+}
+
+export const useCameraStore = create<TemporalCameraStoreState>()((set) => ({
+  selectedImage: [],
+  addSelectedImage: (image: string) =>
+    set((state) => ({ selectedImage: [...state.selectedImage, image] })),
+  clearImages: () => set({ selectedImage: [] }),
+}));
